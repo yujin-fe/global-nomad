7d580730559e72d5f082f6ac985a0e794f7e126f
yujin-fe <hyujin1224@gmail.com>
2025-12-22 13:22:27 +0000

    [✨ Feature] 프로필 이미지 컴포넌트 (#32)
    Co-authored-by: 양은지 <eunji124@github.com>

---
+++ src/components/Profile.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { cva } from 'class-variance-authority';
+import Image from 'next/image';
+
+import defaultProfile from '@/assets/images/common/img-profile.svg';
+import { cn } from '@/util/cn';
+
+const profileVariants = cva('rounded-full object-cover', {
+  variants: {
+    size: {
+      lg: 'w-[120px] h-[120px]',
+      md: 'w-[70px] h-[70px]',
+      sm: 'w-[30px] h-[30px]',
+    },
+  },
+  defaultVariants: {
+    size: 'sm',
+  },
+});
+
+interface ProfileProps {
+  src?: string;
+  alt?: string;
+  size?: 'lg' | 'md' | 'sm';
+  className?: string;
+}
+
+export default function Profile({
+  src,
+  alt = 'profile',
+  size,
+  className,
+}: ProfileProps) {
+  return (
+    <div className={cn(profileVariants({ size }), className, 'relative')}>
+      <Image
+        src={src || defaultProfile}
+        alt={alt}
+        fill
+        className="rounded-full object-cover"
+      />
+    </div>
+  );
+}
